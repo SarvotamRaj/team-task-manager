ca11949f56d16749593302e134ebadc2bb4e557e
Sarvotam Raj <01sarvotam@gmail.com>
2026-06-01 09:51:05 +0530

    Fix dashboard service duplicate

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -1,21 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/dashboard";
-
-export const getDashboardData = async () => {
-
-  const token = localStorage.getItem("token");
-
-  const response = await axios.get(API_URL, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  return response.data;
-};import axios from "axios";
-
-const API_URL = "https://team-task-manager-production-a0ee.up.railway.app/api/dashboard";
+const API_URL =
+  "https://team-task-manager-production-a0ee.up.railway.app/api/dashboard";
 
 export const getDashboardData = async () => {
   const token = localStorage.getItem("token");
