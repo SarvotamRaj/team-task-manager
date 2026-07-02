de358a69aa205afdf702d4200157be12a59f984e
Sarvotam Raj <01sarvotam@gmail.com>
2026-07-02 18:35:24 +0530

    Add task routes and fix CORS

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
-const testRoutes = require("./routes/testRoutes");
-const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
-const dashboardRoutes = require("./routes/dashboardRoutes");
 
 dotenv.config();
 
@@ -16,14 +13,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/test", testRoutes);
-app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use("/api/dashboard", dashboardRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Running");
